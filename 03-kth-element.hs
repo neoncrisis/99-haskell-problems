fcc1a9c89b0ff12afdd42957e76a907dbadfc050
Oz <osvaldo.lopez@superluminal.me>
2015-10-16 19:56:11 -0600

    Third Problem - handle out of bounds case

--- 03-kth-element.hs
+++ 03-kth-element.hs
@@ -9,10 +9,11 @@
 import Helpers
 
 elementAt :: [a] -> Int -> a
-elementAt [] _     = undefined
-elementAt _  0     = undefined
-elementAt (x:xs) 1 = x
-elementAt (x:xs) n = elementAt xs (n-1)
+elementAt [] _     = error "Index out of bounds"
+elementAt (x:_) 1  = x
+elementAt (_:xs) n
+  | n < 1          = error "Index out of bounds"
+  | otherwise      = elementAt xs (n-1)
 
 main = runTests [
     elementAt [1, 2, 3] 2 @?= 2
